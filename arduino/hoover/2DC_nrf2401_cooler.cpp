a25c3b9e520ff6656f4afcce10925e5e6ceb9dc3
Anton Davedenko <davedenko83@gmail.com>
2017-03-22 13:36:59 +0300

    Update 2DC_nrf2401_cooler.cpp

--- arduino/hoover/2DC_nrf2401_cooler.cpp
+++ arduino/hoover/2DC_nrf2401_cooler.cpp
@@ -1,12 +1,20 @@
 //# hoover
 
-int IN1 = 2; // Input1 подключен к выводу 2 
+int IN1 = 2; // Input1 подключен к выводу 2
 int IN2 = 3;
 int IN3 = 4;
 int IN4 = 7;
 int ENA = 6;
 int ENB = 5;
 int i;
+// задаем константы
+const int buttonPin = 8;     // номер входа, подключенный к кнопке
+const int ledPin =  13;      // номер выхода светодиода
+
+// переменные
+int buttonState = 0;         // переменная для хранения состояния кнопки
+
+
 void setup()
 {
   pinMode (ENA, OUTPUT);
@@ -15,47 +23,51 @@ void setup()
   pinMode (ENB, OUTPUT);
   pinMode (IN4, OUTPUT);
   pinMode (IN3, OUTPUT);
-  
+
   pinMode (13, OUTPUT);
-  
- 
+  // инициализируем пин, подключенный к кнопке, как вход
+  pinMode(8, INPUT);
+
+
 }
 void loop()
 {
-  
-  digitalWrite (IN2, HIGH);
-  digitalWrite (IN1, LOW); 
-  digitalWrite (IN4, HIGH);
-  digitalWrite (IN3, LOW); 
-
-analogWrite(ENA, 150);
-      analogWrite(ENB, 150);
-      delay(3000);
 
 
+  // считываем значения с входа кнопки
+  buttonState = digitalRead(buttonPin);
 
-  /*
-  for (i = 50; i <= 180; ++i)
-  {
-      analogWrite(ENA, i);
-      analogWrite(ENB, i);
-      delay(30);
+  // проверяем нажата ли кнопка
+  // если нажата, то buttonState будет HIGH:
+  if (buttonState == HIGH) {
+    // включаем светодиод
+    wall();
   }
-  analogWrite (ENA, 0);
-  analogWrite (ENB, 0);
-  delay(500);
+  else {
+    // выключаем светодиод
+    go();
+  }
+
+}
+
+void go() {
+  digitalWrite (IN1, LOW);
+  digitalWrite (IN2, HIGH);
+  digitalWrite (IN3, LOW);
+  digitalWrite (IN4, HIGH);
+  analogWrite(ENA, 90);
+  analogWrite(ENB, 90);
+}
+
+void wall() {
   digitalWrite (IN1, HIGH);
-  digitalWrite (IN2, LOW); 
+  digitalWrite (IN2,LOW);
   digitalWrite (IN3, HIGH);
   digitalWrite (IN4, LOW);
-  for (i = 50; i <= 180; ++i)
-  {
-      analogWrite(ENA, i);
-      analogWrite(ENB, i);
-      delay(30);
-  }
-  analogWrite (ENA, 0);
-  analogWrite (ENB, 0);
-  delay(8000);
-  */
+  analogWrite(ENA, 100);
+  analogWrite(ENB, 100);
+  delay(500);
+  digitalWrite (IN3,LOW);
+  digitalWrite (IN4, HIGH);
+  delay(500);
 }
